de858c5f44c5c31f9b5cd2cef4d2ad66a6cbdd73
accinn <aylinpt02@gmail.com>
2024-09-29 04:02:49 -0300

    agrego form

---
+++ TPFinalNivel2_Paniagua/FormPrincipal.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace TPFinalNivel2_Paniagua
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.pbxImagen = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxImagen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 75);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(543, 226);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // pbxImagen
+            // 
+            this.pbxImagen.Location = new System.Drawing.Point(571, 75);
+            this.pbxImagen.Name = "pbxImagen";
+            this.pbxImagen.Size = new System.Drawing.Size(221, 225);
+            this.pbxImagen.TabIndex = 1;
+            this.pbxImagen.TabStop = false;
+            // 
+            // FormPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pbxImagen);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "FormPrincipal";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxImagen)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.PictureBox pbxImagen;
+    }
+}
+
---
+++ negocio/AccesoDatos.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace negocio
+{
+    public class AccesoDatos
+    {
+        private SqlConnection conexion;
+
+        private SqlCommand comando;
+        private SqlDataReader lector;
+        private string consulta; //aca guarda la consulta a llamar
+
+        public SqlDataReader Lector
+        {
+            get { return lector; }
+        }
+        public AccesoDatos() //creo su constructor
+        {
+            conexion = new SqlConnection("server=¨.\\SQLEXPRESS; database=CATALOGO_DB; integrated security=true");
+            comando = new SqlCommand();
+        }
+        public void SetearConsulta(string Consulta)
+        {
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = Consulta;
+        }
+        public void EjecutarLectura()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                lector = comando.ExecuteReader();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+        public void ejecutarAccion()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public void SetearParametro(string nombre, object valor)
+        {
+            comando.Parameters.AddWithValue(nombre, valor);
+        }
+
+        public void CerrarConeccion()
+        {
+            if (lector != null) { lector.Close(); }
+            conexion.Close();
+        }
+
+        public object EjecutarEscalar ()
+        {
+            try
+            {
+                if (conexion.State != System.Data.ConnectionState.Open)
+                {
+                    conexion.Open();
+                }
+
+                return comando.ExecuteScalar(); 
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            {
+                if (conexion.State == System.Data.ConnectionState.Open)
+                {
+                    conexion.Close();
+                }
+            }
+        }
+    }
+}
